073be41f132a24180979ea87732c41a267befe71
Gaziz <gazizbolat2@gmail.com>
2024-01-19 10:13:10 +0600

    task: add variables for w3

---
+++ lab1/w3-variables.py
@@ -0,0 +1,23 @@
+# Dynamic variables
+x = 5
+print(x)
+x = 'string'
+print(x)
+
+# Casting data types
+x = str(5)
+y = int(5)
+z = float(5)
+print(x, y, z)
+
+# Get data type
+x = 5
+m = "string"
+print(type(x))
+print(type(m))
+
+# Case sensitive
+
+a = 5
+A = 10
+print(a, A)
