f88bea87f989b4b3fda9a8b949daa2c356364b4b
Gaziz <gazizbolat2@gmail.com>
2024-01-25 00:03:45 +0600

    task: add data-types for w3

---
+++ lab1/w3-data-types.py
@@ -0,0 +1,62 @@
+# str
+str = "Hello World!"
+print(type(str))
+
+# int
+int = 5
+print(type(int))
+
+# float
+float = 5.0
+print(type(float))
+
+# complex
+complex = 5 + 5j
+print(type(complex))
+
+# list
+list = [1, 2, 3]
+print(type(list))
+
+# tuple
+tuple = (1, 2, 3)
+print(type(tuple))
+
+# range
+range = range(10)
+print(type(range))
+
+# dict
+dict = {"name": "John", "age": 20}
+print(type(dict))
+
+# set
+set = {1, 2, 3, 4, 5}
+print(type(set))
+
+# frozenset
+frozenset = frozenset({1, 2, 3, 4, 5})
+print(type(frozenset))
+
+# bool
+bool = True
+print(type(bool))
+
+# bytes
+bytes = b"Hello World!"
+print(type(bytes))
+print(bytes)
+
+# bytearray
+bytearray = bytearray(b"Hello World!")
+print(type(bytearray))
+print(bytearray)
+
+# memoryview
+memoryview = memoryview(b"Hello World!")
+print(type(memoryview))
+print(memoryview)
+
+# None
+none = None
+print(type(none))
