9bf230e5c357d78138a5eba7c3c5348cf9eba64b
Gaziz <gazizbolat2@gmail.com>
2024-02-08 00:54:37 +0600

    task: add conditional-statement for w3

---
+++ lab2/w3-conditional statement.py
@@ -0,0 +1,28 @@
+# if
+if 5 > 2:
+    print("5 is greater than 2")
+
+# if else
+if 5 > 2:
+    print("5 is greater than 2")
+else:
+    print("5 is not greater than 2")
+
+# if elif else
+if 5 > 2:
+    print("5 is greater than 2")
+elif 5 == 2:
+    print("5 is equal to 2")
+else:
+    print("5 is not greater than 2")
+
+# Short hand if
+if 5 > 2: print("5 is greater than 2")
+
+# Short hand if else
+print("5 is greater than") if 5 > 2 else print("5 is less than")
+
+# AND NOT OR
+if 5 > 2 and 2 > 1: print("5 is greater than 2")
+if not 5 < 2 and 2 > 1: print("5 is greater than 2")
+if 5 < 2 or 2 > 1: print("5 is greater than 2")
