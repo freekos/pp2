0a7176d650186251633bfa576674b2097678d333
freekos <gazizbolat2@gmail.com>
2024-04-12 03:46:52 +0500

    feat: change utils for lab-7

--- lab7/utils.py
+++ lab7/utils.py
@@ -2,7 +2,19 @@ import pygame as py
 import os
 
 def load_image(path: str) -> py.Surface:
-    root = os.getcwd()
-    file_path = os.path.join(f"{root}/assets", path)
+    file_path = get_file_path(path)
     image = py.image.load(file_path)
     return image
+
+def load_music(path: str):
+    file_path = get_file_path(path)
+    py.mixer.music.load(file_path)
+
+def get_file_path(path: str) -> str:
+    assets_path = get_assets_path()
+    file_path = os.path.join(assets_path, path)
+    return file_path
+
+def get_assets_path():
+    root = os.getcwd()
+    return f"{root}/assets"
