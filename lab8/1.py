c316e6bffd4ff89eca09c18af82c352007c7421e
freekos <gazizbolat2@gmail.com>
2024-04-16 17:47:01 +0500

    refactor: change lab-8 first task

--- lab8/1.py
+++ lab8/1.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame as py
 import random
 import sys
 import time
@@ -6,10 +6,10 @@ from pygame.locals import *
 
 import utils
 
-pygame.init()
+py.init()
 
 FPS = 60
-FramePerSec = pygame.time.Clock()
+FramePerSec = py.time.Clock()
 
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
@@ -23,23 +23,23 @@ SPEED = 5
 SCORE = 0
 COIN_SCORE = 0
 
-font = pygame.font.SysFont("Verdana", 60)
-font_small = pygame.font.SysFont("Verdana", 20)
+font = py.font.SysFont("Verdana", 60)
+font_small = py.font.SysFont("Verdana", 20)
 game_over = font.render("Game Over", True, BLACK)
 
-DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Game")
+DISPLAYSURF = py.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+py.display.set_caption("Game")
 
 background = utils.load_image("Road.png")
 player_img = utils.load_image("Player.png")
 enemy_img = utils.load_image("Enemy.png")
 coin_img = utils.load_image("Coin.svg")
 
-enemies = pygame.sprite.Group()
-coins = pygame.sprite.Group()
-all_sprites = pygame.sprite.Group()
+enemies = py.sprite.Group()
+coins = py.sprite.Group()
+all_sprites = py.sprite.Group()
 
-class Player(pygame.sprite.Sprite):
+class Player(py.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = player_img
@@ -47,7 +47,7 @@ class Player(pygame.sprite.Sprite):
         self.rect.center = (160, 520)
 
     def update(self):
-        pressed_keys = pygame.key.get_pressed()
+        pressed_keys = py.key.get_pressed()
         if self.rect.left > 0:
             if pressed_keys[K_LEFT]:
                 self.rect.move_ip(-5, 0)
@@ -56,12 +56,12 @@ class Player(pygame.sprite.Sprite):
                 self.rect.move_ip(5, 0)
 
     def collect_coin(self, coins):
-        collisions = pygame.sprite.spritecollide(self, coins, True)
+        collisions = py.sprite.spritecollide(self, coins, True)
         for coin in collisions:
             return True
         return False
 
-class Enemy(pygame.sprite.Sprite):
+class Enemy(py.sprite.Sprite):
     def __init__(self, start, end):
         super().__init__()
         self.image = enemy_img
@@ -78,7 +78,7 @@ class Enemy(pygame.sprite.Sprite):
             self.rect.top = 0
             self.rect.center = (random.randint(self.start, self.end), 0)
 
-class Coin(pygame.sprite.Sprite):
+class Coin(py.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = coin_img
@@ -106,9 +106,9 @@ coins.add(coin)
 all_sprites.add(coin)
 
 while True:
-    for event in pygame.event.get():
+    for event in py.event.get():
         if event.type == QUIT:
-            pygame.quit()
+            py.quit()
             sys.exit()
 
     player.update()
@@ -121,17 +121,17 @@ while True:
         coins.add(new_coin)
         all_sprites.add(new_coin)
 
-    if pygame.sprite.spritecollideany(player, enemies):
+    if py.sprite.spritecollideany(player, enemies):
         time.sleep(0.5)
 
         DISPLAYSURF.fill(RED)
         DISPLAYSURF.blit(game_over, (30, 250))
 
-        pygame.display.update()
+        py.display.update()
         for entity in all_sprites:
             entity.kill()
         time.sleep(2)
-        pygame.quit()
+        py.quit()
         sys.exit()
 
     DISPLAYSURF.blit(background, (0, 0))
@@ -142,5 +142,5 @@ while True:
     DISPLAYSURF.blit(scores, (10, 10))
     DISPLAYSURF.blit(coin_scores, (375, 10))
 
-    pygame.display.update()
+    py.display.update()
     FramePerSec.tick(FPS)
